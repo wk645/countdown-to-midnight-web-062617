2a1faabca38cd8d096413c191d85c622ec9c3f25
wk645 <wk645@nyu.edu>
2017-06-12 14:05:40 -0400

    countdown lab

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(integer)
+    while integer > 0
+        puts "#{integer} SECOND(S)!"
+        integer -= 1
+        end
+    "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(integer)
+    while integer > 0
+        puts "#{integer} SECOND(S)!"
+        integer -= 1
+        sleep(1)
+        end
+    "HAPPY NEW YEAR!"
 end
